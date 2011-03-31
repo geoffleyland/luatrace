40a7023ee9a4093c8ea4dee87b76768e0ffbac39
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-03-31 15:57:08 +1300

    better calculation of max_time

--- lua/luatrace/profile.lua
+++ lua/luatrace/profile.lua
@@ -3,12 +3,13 @@ local trace_file = require("luatrace.trace_file")
 local source_files
 local stack
 local stack_top
+local total_time
 
 local profile = {}
 
 
 function profile.open()
-  source_files, stack, stack_top = {}, {}, 0
+  source_files, stack, stack_top, total_time = {}, {}, 0, 0
 end
 
 
@@ -48,6 +49,8 @@ function profile.record(a, b, c)
   else
     local line, time = a, b
     line, time = tonumber(line), tonumber(time)
+    total_time = total_time + time
+    
     local top = stack[stack_top]
     local r = top.file.lines[line]
     if not r then
@@ -67,16 +70,33 @@ end
 function profile.close()
   local all_lines = {}
 
+  -- collect all the lines
+  local max_visits = 0
   for _, f in pairs(source_files) do
     for i, l in pairs(f.lines) do
       all_lines[#all_lines + 1] = { filename=f.name, line_number=i, line=l }
+      max_visits = math.max(max_visits, l.visits)
     end
   end
-
   table.sort(all_lines, function(a, b) return a.line.self_time + a.line.child_time > b.line.self_time + b.line.child_time end)
+  local max_time = all_lines[1].line.self_time + all_lines[1].line.child_time
+  
+  local divisor, time_units
+  if max_time < 10000 then
+    divisor = 1
+    time_units = "microseconds"
+  elseif max_time < 10000000 then
+    divisor = 1000
+    time_units = "milliseconds"
+  else
+    io.stderr:write("Times in seconds\n")
+    divisor = 1000000
+    time_units = "seconds"
+  end
+  
 
 
-  local title_len, max_time = 0, 0
+  local title_len = 0
   local file_lines = {}
   for i = 1, math.min(20, #all_lines) do
     local l = all_lines[i]
@@ -84,8 +104,6 @@ function profile.close()
     l.title = ("%s:%d"):format(l.filename, l.line_number)
     title_len = math.max(title_len, l.title:len())
     
-    max_time = math.max(max_time, l.line.self_time + l.line.child_time)
-
     -- Record the lines of the files we want to see
     local fl = file_lines[l.filename]
     if not fl then
@@ -97,7 +115,7 @@ function profile.close()
 
   -- Find the text of the lines
   for file_name, line_numbers in pairs(file_lines) do
-    local f = assert(io.open(file_name, "r"))
+    local f = io.open(file_name, "r")
     if f then
       local i = 1
       for l in f:lines() do
@@ -111,17 +129,8 @@ function profile.close()
     f:close()
   end
 
-  local divisor
-  if max_time < 10000 then
-    io.stderr:write("Times in microseconds\n")
-    divisor = 1
-  elseif max_time < 10000000 then
-    io.stderr:write("Times in milliseconds\n")
-    divisor = 1000
-  else
-    io.stderr:write("Times in seconds\n")
-    divisor = 1000000
-  end
+  io.stderr:write("Times in ", time_units, "\n")
+  io.stderr:write(("Total time %.2f\n"):format(total_time / divisor))
 
   header_format = ("%%-%ds%%8s%%12s%%12s%%12s  Line\n"):format(title_len+2)
   line_format = ("%%-%ds%%8d%%12.2f%%12.2f%%12.2f  %%-s\n"):format(title_len+2)
