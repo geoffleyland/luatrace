5ef214b8aaee59c3ac9f06597b8d647948243ad8
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-04-02 08:01:04 +1300

    two more tests, one of which (one-line-horror) pins down a problem I don't yet understand

---
+++ lua/test/inner-function.lua
@@ -0,0 +1,15 @@
+-- Does time in inner functions get accumulated properly?
+
+luatrace = require("luatrace")
+
+function c(f)
+  for i = 1, 5 do f() end
+end
+
+function a()
+  c(function b() print("wait") end)
+end
+
+luatrace.tron()
+a(5)
+luatrace.troff()
