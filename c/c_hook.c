03db121cccb163b25257ac84ca587e8e362e8ee2
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-04-03 17:49:09 +1200

    The c hook is more careful about calculating times in microseconds (it checks CLOCKS_PER_SEC), and it's getting ready for high resolution timers

--- c/c_hook.c
+++ c/c_hook.c
@@ -1,18 +1,47 @@
-#include <time.h>
 #include "lauxlib.h"
+
+#include <time.h>
+typedef clock_t hook_time_t;
+
 
 /*============================================================================*/
 
-static clock_t time_out, elapsed;
-static int hook_index = -1;
+static hook_time_t microseconds_numerator, microseconds_denominator;
+
+
+static void get_microseconds_info(void)
+{
+ if (CLOCKS_PER_SEC < 1000000)
+  {
+    microseconds_numerator = 1000000 / CLOCKS_PER_SEC;
+    microseconds_denominator = 1;
+  }
+  else
+  {
+    microseconds_numerator = 1;
+    microseconds_denominator = CLOCKS_PER_SEC / 1000000;
+  }
+}
+
+
+static lua_Number convert_to_fp_microseconds(hook_time_t t)
+{
+  return (lua_Number)((microseconds_numerator * t) / microseconds_denominator);
+}
+
+
+/*============================================================================*/
+
 static const char *const hooknames[] = {"call", "return", "line", "count", "tail return"};
+static int hook_index = -1;
+static hook_time_t time_out, elapsed;
 
 
 void hook(lua_State *L, lua_Debug *ar)
 {
-  clock_t time_in = clock();
+  hook_time_t time_in = clock();
   elapsed += time_in - time_out;
-  
+
   int event = ar->event;
   lua_rawgeti(L, LUA_REGISTRYINDEX, hook_index);
 
@@ -23,9 +52,9 @@ void hook(lua_State *L, lua_Debug *ar)
   else
     lua_pushnil(L);
 
-  lua_pushinteger(L, elapsed);
+  lua_pushnumber(L, convert_to_fp_microseconds(elapsed));
   lua_call(L, 3, 0);
-    
+
   elapsed = 0;
   time_out = clock();
 }
@@ -44,6 +73,8 @@ static int set_hook(lua_State *L)
   }
   else
   {
+    get_microseconds_info();
+    
     luaL_checktype(L, 1, LUA_TFUNCTION);
     hook_index = luaL_ref(L, LUA_REGISTRYINDEX);
     lua_sethook(L, hook, LUA_MASKCALL | LUA_MASKRET | LUA_MASKLINE, 0);
